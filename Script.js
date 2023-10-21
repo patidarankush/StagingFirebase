6a6ee8ddc34d8d73a6c9e1dd49764bbd8d027337
patidarankush <patidarankush59@gmail.com>
2023-10-21 18:50:31 +0530

    added tokenbalance

--- Script.js
+++ Script.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
     messagingSenderId: "549481442193",
     appId: "1:549481442193:web:b0e1428b63ad6b81dd4830",
     measurementId: "G-JRYK4Z6M9J"
-  };
+};
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
@@ -22,6 +22,8 @@ const VRScene1 = document.getElementById('VRScene1');
 const VRScene2 = document.getElementById('VRScene2');
 const VRScene3 = document.getElementById('VRScene3');
 const Scenestart = document.getElementById('Scenestart');
+const tokenBalance = document.getElementById('Tokenbalanceinput');
+const tokenBalancebutton = document.getElementById('Tokenbalance');
 
 // Fetch the initial state from Firebase for switchState
 database.ref('switchState').on('value', (snapshot) => {
@@ -65,6 +67,16 @@ database.ref('Scenestart').on('value', (snapshot) => {
     updateButton(Scenestart, state);
 });
 
+// Fetch the initial state from Firebase for Tokenbalance
+// Reference to the "tokenbalance" child element under "playerprofile"
+const playerProfileRef = database.ref('Playerprofile/Tokenbalance');
+
+// Read the value of "tokenbalance"
+playerProfileRef.on('value', (snapshot) => {
+    const state = snapshot.val();
+    updateButtontwo(tokenBalance, state, tokenBalancebutton);
+});
+
 
 // Update button based on the state
 function updateButton(buttonElement, state) {
@@ -73,9 +85,25 @@ function updateButton(buttonElement, state) {
     buttonElement.disabled = false;
 }
 
+// Update textinput based on the value and handle the button click event
+function updateButtontwo(textelement, value, buttonElement) {
+    textelement.value = value; // Use .value to set the input field's value
+    buttonElement.onclick = () => updateTokenBalance(textelement);
+    textelement.disabled = false;
+}
+
 // Toggle the state in Firebase
 function toggleState(refName, newState) {
     const buttonElement = document.getElementById(refName);
     buttonElement.disabled = true;
     database.ref(refName).set(newState);
+}
+
+// Function to update the Token Balance in Firebase
+function updateTokenBalance(inputElement) {
+    const newTokenBalance = inputElement.value;
+    const refName = 'Playerprofile/Tokenbalance';
+
+    // Update the value in Firebase
+    database.ref(refName).set(newTokenBalance);
 }
